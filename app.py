27b2f113b73beb93282a83b40746244e0b8ae1fd
Claybson Jordão <oninoniisan@gmail.com>
2024-09-05 10:46:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,26 +44,7 @@ def login_psicologo():
             
     return render_template('login_psicologo.html')
 
-#testando
-@app.route('/telapsicologo', methods=['GET', 'POST'])
-def telapsicologo_login():
-    """if request.method == 'POST':
-        email = request.form.get('email')
-        senha = request.form.get('senha')
-        
-        if not email or not senha:
-            flash('Todos os campos são obrigatórios.', 'danger')    
-            return render_template('login.html')
 
-        user = sqlite.check_login(email, senha)
-        if user:
-            session['user_id'] = user['id']
-            return redirect(url_for('telapsicologo'))
-        else:
-            flash('Email ou senha inválidos', 'danger')"""
-
-    return render_template('telapsicologo.html')
-#testando
 @app.route('/telapaciente')
 def telapaciente():
     if 'user_id' not in session:
@@ -82,14 +63,14 @@ def cadastro():
         email = request.form.get('email')
         senha = request.form.get('senha')
         data_nascimento = request.form.get('data_nascimento')
-        #cpf = request.form.get('cpf')
+        cpf = request.form.get('cpf')
         matricula = request.form.get('matricula')
 
-        if not nome or not email or not senha or not data_nascimento or not matricula:
+        if not nome or not email or not senha or not data_nascimento or not cpf or not matricula:
             flash('Todos os campos são obrigatórios.', 'danger')
             return render_template('cadastro.html')
 
-        sqlite.insert_paciente(nome, email, senha, data_nascimento, matricula)
+        sqlite.insert_paciente(nome, email, senha, data_nascimento, cpf, matricula)
         return redirect(url_for('home'))
 
     return render_template('cadastro.html')
@@ -110,6 +91,7 @@ def anotacao_paciente():
         return redirect(url_for('home'))
 
     return render_template('anotacao_paciente.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
